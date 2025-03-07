9049134f10817b0d7197dda70fe38fca3907524e
David Vanjao <davidrafaeldj@hotmail.com>
2025-03-07 04:26:28 -0300

    Melhorando a implementacao do metodo imprimirTabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -6,26 +6,28 @@ namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
 
-            PosicaoXadrez pos = new PosicaoXadrez('b', 1); //coluna e linha
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosicao());
-            Console.ReadLine();
+            //PosicaoXadrez pos = new PosicaoXadrez('c', 3); //coluna e linha
+            //Console.WriteLine(pos);
+            //Console.WriteLine(pos.toPosicao());
+            //Console.ReadLine();
 
-            //try {
+            try {
 
-            //    Tabuleiro tab = new Tabuleiro(8, 8);
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            //    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            //    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            //    tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-            //    Tela.imprimirTabuleiro(tab);
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
 
-            //    Console.ReadLine();
+                Tela.imprimirTabuleiro(tab);
 
-            //} catch (Exception e) { //caso aconteca algum erro em try, exibi a mensagem de erro.
-            //    Console.WriteLine(e.Message);
-            //}
+                Console.ReadLine();
+
+            } catch (Exception e) { //caso aconteca algum erro em try, exibi a mensagem de erro.
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -6,15 +6,30 @@ namespace xadrez_console {
     internal class Tela {
         public static void imprimirTabuleiro(Tabuleiro tab) { //elemenstos staticos
             for (int i = 0; i < tab.linhas; i++) {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.colunas; j++) {
                     if (tab.peca(i, j) == null) {
                         Console.Write("- ");
                     } else {
-                        Console.Write(tab.peca(i, j) + " ");//colunas
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine(); //quebra a linha
             }
+            Console.Write("  " + "a " + "b " + "c " + "d " + "e " + "f " + "g " + "h ");
         }
+
+        public static void imprimirPeca(Peca peca) {
+            if (peca.cor == Cor.Branca) {
+                Console.Write(peca);
+            } else {
+                ConsoleColor aux = Console.ForegroundColor; //Salva a cor atual do console
+                Console.ForegroundColor = ConsoleColor.Yellow; //Muda a cor do texto para amarelo
+                Console.Write(peca); //Imprime a peça no console em amarelo
+                Console.ForegroundColor = aux; //Restaura a cor original do console
+            }
+        }
+
     }
 }
