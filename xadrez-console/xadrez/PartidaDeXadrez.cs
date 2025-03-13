84f78fb20c3edfd57a2ef6e38aa43b0145e769d3
David Vanjao <davidrafaeldj@hotmail.com>
2025-03-13 05:04:41 -0300

    Implementando a verificação de xeque

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -11,30 +11,60 @@ namespace xadrez {
         public bool terminada {  get; private set; }
         private HashSet<Peca> pecas;
         private HashSet<Peca> capturadas;
+        public bool xeque { get; private set; } //verifica se a partida esta em xeque
 
         public PartidaDeXadrez() {
             tab = new Tabuleiro(8, 8);
             turno = 1;
             jogadorAtual = Cor.Branca;
+            terminada = false;
+            xeque = false;
             pecas = new HashSet<Peca>();
             capturadas = new HashSet<Peca>();
             colocarPecas();
         }
 
-        public void executaMovimento(Posicao origem, Posicao destino) {
+        public Peca executaMovimento(Posicao origem, Posicao destino) {
             Peca p = tab.retirarPeca(origem);
             p.incrementarQteMovimentos();
             Peca pecaCapturada = tab.retirarPeca(destino);
             tab.colocarPeca(p, destino);
 
             if (pecaCapturada != null) {
-                capturadas.Add(pecaCapturada); //adiciona peca capturada a lista de capturadas
+                capturadas.Add(pecaCapturada); //adiciona peca capturada ao conjunto de capturadas
             }
+
+            return pecaCapturada;
+
+        }
+
+        public void desfazMovimento(Posicao origem, Posicao destino, Peca pecaCapturada) { //desfaz o ultimo movimento se a peca estiver em xeque
+            Peca p = tab.retirarPeca(destino);
+            p.decrementarQteMovimentos();
+            if (pecaCapturada != null) {
+                tab.colocarPeca(pecaCapturada, destino);
+                capturadas.Remove(pecaCapturada);
+            }
+            tab.colocarPeca(p, origem);
 
         }
 
         public void realizaJogada(Posicao origem, Posicao destino) {
-            executaMovimento(origem, destino);
+            Peca pecaCapturada = executaMovimento(origem, destino);
+
+            if (estaEmXeque(jogadorAtual)) { //verifica se a peca esta em xeque
+                desfazMovimento(origem, destino, pecaCapturada);
+                throw new TabuleiroException("Você não pode se colocar em xeque!");
+            }
+
+            if (estaEmXeque(adversaria(jogadorAtual))) {
+                xeque = true;
+            } else {
+                xeque = false;
+            }
+
+
+
             turno++;
             mudaJogador();
         }
@@ -87,6 +117,37 @@ namespace xadrez {
             return aux;
         }
 
+        private Cor adversaria(Cor cor) { //retorna a cor do adversario
+            if (cor == Cor.Branca) {
+                return Cor.Preta;
+            } else {
+                return Cor.Branca;
+            }
+        }
+
+        private Peca rei(Cor cor) { // busca o rei de uma determinada cor no tabuleiro.
+            foreach (Peca x in pecasEmJogo(cor)) {
+                if (x is Rei) {
+                    return x;
+                }
+            }
+            return null;
+        }
+
+        public bool estaEmXeque(Cor cor) { //verifica se o rei da cor informada está em xeque.
+            Peca R = rei(cor);
+            if (R == null) {
+                throw new TabuleiroException("Não tem rei da cor " + cor + " no tabuleiro!");
+            }
+            foreach(Peca x in pecasEmJogo(adversaria(cor))) { //Percorre todas as peças adversárias
+                bool[,] mat = x.movimentosPossiveis(); //Obtém os movimentos possíveis da peça adversária
+
+                if (mat[R.posicao.linha, R.posicao.coluna]) { // Se a posição do rei está dentro dos movimentos possíveis, ele está em xeque. verifica se o meu reio esta nas posicoes possiveis da peca do adversario
+                    return true;
+                }
+            }
+            return false;
+        }
 
         public void colocarNovaPeca(char coluna, int linha, Peca peca) {
             tab.colocarPeca(peca, new PosicaoXadrez(coluna, linha).toPosicao());
